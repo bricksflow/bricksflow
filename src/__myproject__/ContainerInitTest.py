9aca16e4c9cab53ad6df7b9f2526891f7e8bf215
Jiri Koutny <jirkakoutny@gmail.com>
2020-05-09 07:09:10 +0200

    ContainerInitTest must test the "dev" environment
    It does not make much sense to use it to test the "test" environment

--- src/__myproject__/ContainerInitTest.py
+++ src/__myproject__/ContainerInitTest.py
@@ -1,4 +1,3 @@
-import os
 import unittest
 from injecta.testing.servicesTester import testServices
 from __myproject__.ContainerInit import initContainer
@@ -6,8 +5,7 @@ from __myproject__.ContainerInit import initContainer
 class ContainerInitTest(unittest.TestCase):
 
     def test_init(self):
-        appEnv = os.environ['APP_ENV']
-        container = initContainer(appEnv)
+        container = initContainer('dev')
 
         testServices(container)
 
