647afaea142be501b3dfeba38d6b782a89e30e19
Jiri Koutny <jirkakoutny@gmail.com>
2021-02-05 16:35:10 +0100

    black fixed

--- src/__myproject__/bronze/covid/tbl_template_1_mask_usage/tbl_template_1_mask_usage.py
+++ src/__myproject__/bronze/covid/tbl_template_1_mask_usage/tbl_template_1_mask_usage.py
@@ -40,6 +40,7 @@ spark.sql(f"create database if not exists {os.environ['APP_ENV']}_gold_reporting
 
 # COMMAND ----------
 
+
 @dataFrameLoader(tableParams("bronze_covid.tbl_template_1_mask_usage").source_csv_path)
 def read_csv_mask_usage(source_csv_path: str, spark: SparkSession, logger: Logger):
     logger.info(f"Reading CSV from source path: `{source_csv_path}`.")
@@ -51,16 +52,19 @@ def read_csv_mask_usage(source_csv_path: str, spark: SparkSession, logger: Logge
         .limit(10)  # only for test
     )
 
+
 # COMMAND ----------
 
 # MAGIC %md #### Using display=True to display transformation outputs
 
 # COMMAND ----------
 
+
 @transformation(read_csv_mask_usage, display=True)
 def add_column_insert_ts(df: DataFrame, logger: Logger):
     logger.info("Adding Insert timestamp")
     return df.withColumn("INSERT_TS", f.lit(datetime.now()))
+
 
 # COMMAND ----------
 
@@ -68,15 +72,16 @@ def add_column_insert_ts(df: DataFrame, logger: Logger):
 
 # COMMAND ----------
 
+
 @dataFrameSaver(add_column_insert_ts)
 def save_table_bronze_covid_tbl_template_1_mask_usage(df: DataFrame, logger: Logger, table_manager: TableManager):
     # Recreate = remove table and create again
     table_manager.recreate("bronze_covid.tbl_template_1_mask_usage")
 
     output_table_name = table_manager.getName("bronze_covid.tbl_template_1_mask_usage")
-    
+
     logger.info(f"Saving data to table: {output_table_name}")
-    
+
     (
         df.select("COUNTYFP", "NEVER", "RARELY", "SOMETIMES", "FREQUENTLY", "ALWAYS", "INSERT_TS")
         .write.option("partitionOverwriteMode", "dynamic")
